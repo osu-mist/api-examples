f63a4c6faa7226cf0dfc8cd2c07888eaecc685bf
Mist Student <miststudent@10-249-199-249.wireless.oregonstate.edu>
2016-04-01 09:19:15 -0700

    bugs fixed. but I got 'invalid access token' msg in my response now

--- python/hello-world.py
+++ python/hello-world.py
@@ -10,10 +10,10 @@ def getAccessToken(url, client_id, client_secret):
 	curl    = pycurl.Curl()
 
 	# Set options 
-	curl.setopt(pycurl.WRITEFUNCTION, storage.write) # CURLOPT_RETURNTRANSFER in PHP
 	curl.setopt(pycurl.URL, url)                     # CURLOPT_URL in PHP
-	curl.setopt(curl.POST, 1)                        # CURLOPT_POST in PHP
-	curl.setopt(curl.POSTFIELDS, post_data)          # CURLOPT_POSTFIELDS in PHP
+	curl.setopt(pycurl.POST, 1)                      # CURLOPT_POST in PHP
+	curl.setopt(pycurl.POSTFIELDS, post_data)        # CURLOPT_POSTFIELDS in PHP
+	curl.setopt(pycurl.WRITEFUNCTION, storage.write) # CURLOPT_RETURNTRANSFER in PHP
 
 	# Send the request and save response
 	curl.perform()
@@ -22,34 +22,34 @@ def getAccessToken(url, client_id, client_secret):
 	curl.close()
 
 	response = storage.getvalue()
-	return json.load(response)
+	return json.loads(response)
+
 
 def getLocations(url, access_token, params):
 	query_params = urllib.urlencode(params)
-	headers = {"Authorization: Bearer " + access_token: None}
 	api_call_url = url + "?" + query_params;
 
 	storage = StringIO.StringIO()
 	curl    = pycurl.Curl()
 
 	# Set options 
-	curl.setopt(pycurl.WRITEFUNCTION, storage.write)
 	curl.setopt(pycurl.URL, api_call_url)
-	curl.setopt(pycurl.HTTPHEADER, headers) # CURLOPT_HTTPHEADER in PHP
+	curl.setopt(pycurl.HTTPHEADER, ['Authorization: Bearer']) # CURLOPT_HTTPHEADER in PHP
+	curl.setopt(pycurl.WRITEFUNCTION, storage.write)
 
 	curl.perform()
 	curl.close()
 
 	response = storage.getvalue()
-	return json.load(response)
+	return json.loads(response)
 
 
 # Read configuration file in JSON format
 config_data_file = open('configuration.json')
-config_data = json.load(config_data_file)
+config_data      = json.load(config_data_file)
 
 base_url         = config_data["hostname"] + config_data["version"] + config_data["api"]
-access_token_url = base_url + config_data["hostname"]
+access_token_url = base_url + config_data["token_endpoint"]
 
 access_token_response = getAccessToken(access_token_url, config_data["client_id"], config_data["client_secret"]);
 
@@ -58,6 +58,6 @@ locations_url = base_url + config_data["locations_endpoint"]
 params        = {"q": "library"}
 
 response = getLocations(locations_url, access_token, params)
-print response
+print "response: ", response
 
 
